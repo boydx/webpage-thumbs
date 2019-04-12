d602bfeb58289c7af59e30f59d9a4642c36b1a29
Boyd <boyd.land@gmail.com>
2019-04-12 11:50:34 -0400

    use promised-based get

--- puppeteer.js
+++ puppeteer.js
@@ -1,39 +1,65 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const axios = require('axios');
 
-const projs = JSON.parse(fs.readFileSync('projects.json', 'utf8'));
+
+const projects = JSON.parse(fs.readFileSync('list.json', 'utf8'));
 let i = 0
 
-for (proj in projs.features) {
+for (p in projects.publish) {
+    console.log(projects.publish[p])
 
-    (async () => {
-        try {
-
-            let screenshotOptions = {
-                path: `graphics/image${i}`,
-                type: 'jpeg'
-            }
+    axios.get(projects.publish[p])
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
     
-            let x = String(projs.features[proj].properties.link)
-
-            const browser = await puppeteer.launch();
-            const page = await browser.newPage();
-            await page.goto(x);
-            page.setViewport({
-                height: 1000,
-                width: 1200
-            })
-            // await page.waitFor(10000)
-            await page.screenshot(screenshotOptions);
-
-            await browser.close();
-            i++
-
-        } 
-        catch (error) {
-            console.log(`dumbass ${error}`)
-        }
-
-    })();
 
 }
+
+function (url) {
+    // make thumbs
+}
+
+// for (proj in projs.features) {
+
+//     (async () => {
+//         try {
+
+//             let screenshotOptions = {
+//                 path: `graphics/image${i}`,
+//                 type: 'jpeg'
+//             }
+    
+//             let x = String(projs.features[proj].properties.link)
+
+//             const browser = await puppeteer.launch();
+//             const page = await browser.newPage();
+//             await page.goto(x);
+//             page.setViewport({
+//                 height: 1000,
+//                 width: 1200
+//             })
+//             // await page.waitFor(10000)
+//             await page.screenshot(screenshotOptions);
+
+//             await browser.close();
+//             i++
+
+//         } 
+//         catch (error) {
+//             console.log(`dumbass ${error}`)
+//         }
+
+//     })();
+
+// }
+//
